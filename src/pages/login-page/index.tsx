f87e8e7c244a1104adae646111d6a34ec81aaeca
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2024-10-23 01:32:04 +0300

    feat: implement test user authorization

--- src/pages/login-page/index.tsx
+++ src/pages/login-page/index.tsx
@@ -1,7 +1,10 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
+
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Link } from '@/components/ui/link';
 import { Logo } from '@/components/ui/logo';
+import { useLoginMutation } from '@/store/auth/authApi';
 
 import {
   Container,
@@ -13,25 +16,62 @@ import {
   SignUpLintWrapper,
 } from './login.styled';
 
+type FormData = {
+  email: string;
+  password: string;
+};
+
 export const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+  const [loginUser, { isLoading, isError, isSuccess, error }] = useLoginMutation();
+
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    try {
+      await loginUser(data).unwrap();
+      alert('Login successful!');
+    } catch (err) {
+      alert('Login failed.');
+    }
+  };
+
   return (
     <Container>
       <Section>
         <LogoWrapper>
           <Logo />
         </LogoWrapper>
-        <LoginForm>
+        <LoginForm onSubmit={handleSubmit(onSubmit)}>
           <FormTitle>Log in to Twitter</FormTitle>
           <ControlGroup>
-            <Input type="email" placeholder="Phone number, email address" required={true} />
-            <Input type="password" placeholder="Password" required={true} />
-            <Button variant="primary" fullWidth={true} size="large">
-              Log in
+            <Input
+              type="email"
+              placeholder="Phone number, email address"
+              fullWidth={true}
+              required={true}
+              {...register('email', { required: 'Email is required' })}
+              error={errors.email?.message}
+            />
+            <Input
+              type="password"
+              placeholder="Password"
+              fullWidth={true}
+              required={true}
+              {...register('password', { required: 'Password is required' })}
+              error={errors.password?.message}
+            />
+            <Button type="submit" variant="primary" fullWidth={true} size="large" disabled={isLoading}>
+              {isLoading ? 'Logging in...' : 'Log in'}
             </Button>
           </ControlGroup>
           <SignUpLintWrapper>
             <Link to="/sign-up">Sign up to Twitter</Link>
           </SignUpLintWrapper>
+          {isError && <p style={{ color: 'red' }}>Login failed: {error?.toString()}</p>}
+          {isSuccess && <p style={{ color: 'green' }}>User logged in successfully!</p>}
         </LoginForm>
       </Section>
     </Container>
