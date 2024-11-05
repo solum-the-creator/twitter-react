0c4a3d1d7c6663cdf75039d22be013d177022a3d
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2024-11-05 23:40:38 +0300

    feat: add navigation to Profile page

--- src/components/sidebar/sidebar-profile/index.tsx
+++ src/components/sidebar/sidebar-profile/index.tsx
@@ -1,12 +1,20 @@
+import { useNavigate } from 'react-router-dom';
+
 import defaultProfile from '@/assets/images/default-profile.png';
 import { CenteredLoader } from '@/components/centered-loader';
 import { ProfileImage } from '@/components/ui/profile-image';
 import { useGetAuthProfile } from '@/hooks/use-get-auth-profile';
+import { getProfilePath } from '@/utils/paths-utils';
 
 import { ProfileContainer, UserEmail, UserInfo, UserName } from './sidebar-profile.styled';
 
 export const SidebarProfile: React.FC = () => {
-  const { userProfile, isLoading } = useGetAuthProfile();
+  const navigate = useNavigate();
+  const { uid, userProfile, isLoading } = useGetAuthProfile();
+
+  const onClick = () => {
+    navigate(getProfilePath(uid));
+  };
 
   if (isLoading) {
     return <CenteredLoader />;
@@ -16,7 +24,7 @@ export const SidebarProfile: React.FC = () => {
   const profileImg = userProfile?.profileImage || defaultProfile;
 
   return (
-    <ProfileContainer>
+    <ProfileContainer onClick={onClick}>
       <ProfileImage size={45} src={profileImg} alt={name} />
       <UserInfo>
         <UserName>{name}</UserName>
